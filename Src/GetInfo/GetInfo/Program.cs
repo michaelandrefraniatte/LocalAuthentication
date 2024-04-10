dc992b43068dc816464537abfcc06cf88f647d70
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-11 00:52:48 +0200

    Add links

---
+++ Src/GetInfo/GetInfo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace GetInfo
+{
+    internal class Program
+    {
+        static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetUniqueID());
+            Console.Read();
+        }
+        public static string GetUniqueID()
+        {
+            try
+            {
+                string cpuInfo = string.Empty;
+                ManagementClass mc = new ManagementClass("win32_processor");
+                ManagementObjectCollection moc = mc.GetInstances();
+                foreach (ManagementObject mo in moc)
+                {
+                    cpuInfo = mo.Properties["processorID"].Value.ToString();
+                    break;
+                }
+                string drive = "C";
+                ManagementObject dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""" + drive + @":""");
+                dsk.Get();
+                string volumeSerial = dsk["VolumeSerialNumber"].ToString();
+                string uuidInfo = string.Empty;
+                ManagementClass mcu = new ManagementClass("Win32_ComputerSystemProduct");
+                ManagementObjectCollection mocu = mcu.GetInstances();
+                foreach (ManagementObject mou in mocu)
+                {
+                    uuidInfo = mou.Properties["UUID"].Value.ToString();
+                    break;
+                }
+                if (volumeSerial != null & volumeSerial != "" & cpuInfo != null & cpuInfo != "" & uuidInfo != null & uuidInfo != "")
+                    return volumeSerial + cpuInfo + uuidInfo;
+                else
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Src/GetMac/GetMac/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetMac
+{
+    internal class Program
+    {
+        static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMAC());
+            Console.Read();
+        }
+        static string GetMAC()
+        {
+            try 
+            {
+                String firstMacAddress = System.Net.NetworkInformation.NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(nic => nic.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up && nic.NetworkInterfaceType != System.Net.NetworkInformation.NetworkInterfaceType.Loopback)
+                .Select(nic => nic.GetPhysicalAddress().ToString())
+                .FirstOrDefault();
+                return firstMacAddress;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
